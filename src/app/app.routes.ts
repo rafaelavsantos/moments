9d2b60f01630f45fa15e0449398ff9ddaf10424b
Rafaela Santos <rafaelasantosminios@gmail.com>
2024-07-19 10:32:48 -0300

    feat: finalizando a estruturação do projeto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NewMomentsComponent } from './pages/new-moments/new-moments.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'sobre', component: AboutComponent },
+  { path: 'compartilhar-moments', component: NewMomentsComponent }
+];
---
+++ src/app/pages/new-moments/new-moments.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-moments',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-moments.component.html',
+  styleUrl: './new-moments.component.css'
+})
+export class NewMomentsComponent {
+
+}
