14da87d409432d1137524ad7182647ed42fe5543
Rafaela Santos <rafaelasantosminios@gmail.com>
2024-07-23 10:55:41 -0300

    feat: finalizando as validações do form e estlizando o erro

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,12 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { NgIf } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './moment-form.component.html',
   styleUrl: './moment-form.component.css'
 })
-export class MomentFormComponent {
+export class MomentFormComponent implements OnInit {
   @Input() btnText!: string;
+
+  momentForm!: FormGroup;
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      titulo: new FormControl('', [Validators.required]),
+      descricao: new FormControl('', [Validators.required]),
+      image: new FormControl('')
+    });
+  }
+
+  get titulo() {
+    return this.momentForm.get('titulo')!;
+  }
+
+  get descricao() {
+    return this.momentForm.get('descricao')!;
+  }
+
+  submit() {
+    if (this.momentForm.invalid) {
+      return;
+    }
+    console.log("Enviou de formulário!")
+  }
 }
