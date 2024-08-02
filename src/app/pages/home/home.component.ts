2d0b9e9ff1799e11e3068f6bfb35f2c86be481e5
Rafaela Santos <rafaelasantosminios@gmail.com>
2024-08-01 21:44:52 -0300

    feat: finalizando a pagina home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Moment } from '../../Moment';
+import { MomentService } from '../../services/moment.service';
+import { NgFor, NgIf } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgFor, NgIf, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  // pegar todos os dados do banco 
+  allMoments: Moment[] = [];
+
+  // filtro que será exibido depois da busca
+  moments: Moment[] = [];
+
+  // rota para api
+  baseApi = 'http://localhost:3333/';
+
+  constructor(private momentService: MomentService) { }
+
+
+  ngOnInit(): void {
+    // Inicializar a função de busca
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data;
+
+      data.map((item) => {
+        item.created_at = item.created_at.slice(0, 10).split("-").reverse().join("/");
+        console.log(item.created_at)
+        // item.created_at = new Date(item.created_at).toLocaleDateString('pt-BR');
+      });
+
+
+      this.allMoments = data;
+      this.moments = data;
+
+      console.log(this.moments)
+    })
+  }
+
 
 }
--- src/app/services/moment.service.ts
+++ src/app/services/moment.service.ts
@@ -1,14 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Response } from '../Response';
+import { Moment } from '../Moment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MomentService {
-  private baseApiUrl = 'http://localhost:3333/'
-  private apiUrl = `${this.baseApiUrl}api/moments`
+  private baseApiUrl = 'http://localhost:3333/';
+  private apiUrl = `${this.baseApiUrl}api/moments`;
+
   constructor(private http: HttpClient) { }
+
+  getMoments(): Observable<Response<Moment[]>> {
+    return this.http.get<Response<Moment[]>>(this.apiUrl);
+  }
 
   createMoment(formData: FormData): Observable<FormData> {
     return this.http.post<FormData>(this.apiUrl, formData);
